f2f8fa18b2713bb8e0be2e6123d33495ca25d548
Alexander de Jong <alexander@dutchcaffeine.nl>
2025-08-09 21:20:08 +0200

    Some types

--- internal/types/types.go
+++ internal/types/types.go
@@ -3,6 +3,10 @@ package types
 
 import "awesome-go/pkgs/srv"
 
+type key int
+
+var UserKey key
+
 type UserForm struct {
 	Name     srv.StringField `field:"name:required,min=5,max=100"`
 	Email    srv.StringField `field:"email:required,email"`
