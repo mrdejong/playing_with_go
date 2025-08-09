5abc19e0645a60e25a45110efbb58d02f29f9536
Alexander de Jong <alexander@dutchcaffeine.nl>
2025-08-09 21:20:29 +0200

    Mixed updates

--- cmd/main.go
+++ cmd/main.go
@@ -2,23 +2,20 @@ package main
 
 import (
 	"awesome-go/internal/handler"
+	"awesome-go/internal/middleware"
 	"awesome-go/internal/service"
 	"awesome-go/pkgs/srv"
-	"fmt"
 	"log"
-	"reflect"
 )
 
 func main() {
-	v := reflect.ValueOf("GTE")
-	fmt.Printf("%v", v.Type())
-
 	app := srv.New()
 
 	s := service.New()
 	h := handler.New(app, s)
+	m := middleware.New(s)
 
-	h.InitializeRoutes()
+	h.InitializeRoutes(m)
 
 	if err := app.Router.Listen(":3000"); err != nil {
 		log.Fatal(err)
