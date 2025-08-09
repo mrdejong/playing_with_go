e51329243d9b144cb1c962d3264f6fe0b742d53a
Alexander de Jong <alexander@dutchcaffeine.nl>
2025-08-09 21:19:44 +0200

    Session model

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,12 +1,27 @@
+// Package models
 package models
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type User struct {
 	gorm.Model
 	Name     string
 	Email    string
 	Password string
+}
+
+type Session struct {
+	gorm.Model
+	UserID    uint
+	User      User `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	Token     string
+	ExpiresOn time.Time
+	UserAgent string
+	IP        string
 }
 
 type TodoStatus string
